37e8e3f064851ac825d66965eabb7d7761929280
Deim Troi <deimdgreat@gmail.com>
2025-09-23 19:33:38 +0800

    basta

--- guestpage/app/Providers/AppServiceProvider.php
+++ guestpage/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        if (app()->environment('local')) {
+        URL::forceScheme('https');
+    }
     }
 }
